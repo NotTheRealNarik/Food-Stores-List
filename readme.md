4527ef16277af6190ebe4cf706aeb40c5a17860b
Harsha Maddipati <maddipati.harsha@gmail.com>
2020-08-07 16:29:18 -0700

    Adding stores to mongaDB

---
+++ readme.md
@@ -0,0 +1,3 @@
+## Food Stores
+A practice project to explore nodeJS, expressJS and mongoDB.
+Technologies and frameworks: NodeJS, ExressJS, mongoDB
---
+++ routes/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const storeController = require('../controllers/storeController')
+const {catchErrors} = require('../handlers/errorHandlers') // imporitng catchErrors mathod
+
+// Do work here
+router.get('/', storeController.homePage);
+router.get('/add', storeController.addStore);
+
+router.post('/add', catchErrors(storeController.createStore));
+
+
+
+router.get('/reverse/:name',(req,res)=>{
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse)
+});
+
+module.exports = router;
